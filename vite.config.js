3e0b237016f5d36192f32d2b318f57851f051db9
Sadikovic Rusmir <sesmanovic@gmail.com>
2023-12-25 21:47:44 +0100

    tsconfig.node.jsonmodif"

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var vite_1 = require("vite");
-var plugin_react_swc_1 = require("@vitejs/plugin-react-swc");
-// Plugins
-var tailwindcss_1 = require("tailwindcss");
-var postcssImport = require("postcss-import");
-var autoprefixer = require("autoprefixer");
-// https://vitejs.dev/config/
-exports.default = (0, vite_1.defineConfig)({
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from 'tailwindcss';
+import postcssImport from 'postcss-import';
+import autoprefixer from 'autoprefixer';
+
+export default defineConfig({
     server: {
         host: '0.0.0.0', // Listen on all network interfaces
         port: 5173, // Optional: Define a specific port
     },
     assetsInclude: ['**/*.ttf'],
-    plugins: [(0, plugin_react_swc_1.default)()],
-    // resolve: {
-    //   alias: {
-    //     '@': resolve(__dirname, 'src'),
-    //   },
-    // },
+    plugins: [react()],
     css: {
         postcss: {
             plugins: [
                 postcssImport(),
-                tailwindcss_1.default,
+                tailwindcss,
                 autoprefixer
             ],
         },
