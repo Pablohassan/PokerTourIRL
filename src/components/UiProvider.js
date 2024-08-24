73ce1863e0648246dd3813cd54362fc7df65387a
Sadikovic Rusmir <sesmanovic@gmail.com>
2024-08-24 19:01:54 +0200

    add uiprovide.js

--- src/components/UiProvider.js
+++ src/components/UiProvider.js
@@ -1,51 +1,62 @@
-// "use strict";
-// import {FC, ReactNode, createContext} from 'react';
-// import {NextUIProvider} from '@nextui-org/react';
-// import toast, {Toaster} from 'react-hot-toast';
-// import React from 'react';
-// // import { useMediaQuery } from "react-responsive";
-// interface UIContextProps {
-//   notify: (type: string, content: string, options?: {}, promiseOptions?: {}) => void;
-// }
-// interface UIProviderProps {
-//   children: ReactNode;
-// }
-// interface UIContextProps {
-//   notify: (type: string, content: string, options?: {}, promiseOptions?: {}) => void;
-//   }
-//   const UIContext = createContext<UIContextProps | undefined>(undefined);
-//   const UIProvider: FC<UIProviderProps> = ({ children }) => {
-//     const notify = (type: string, content:string, options?: {}, promiseOptions?: {}) => {
-//       const toastOptions = {...options};
-//       if (type === 'success') {
-//         return toast.success(content, toastOptions);
-//       }
-//       if (type === 'error') {
-//         return toast.error(content, toastOptions);
-//       }
-//       if (type === 'loading') {
-//         return toast.loading(content, toastOptions);
-//       }
-//       return toast.custom(content, toastOptions);
-//     };
-//     return (
-//       <UIContext.Provider value={{ notify }}>
-//         <NextUIProvider>
-//           <Toaster
-//             toastOptions={{ 
-//               duration: 4000,
-//               position: 'bottom-right',
-//               style: {
-//                 padding: '20px 24px',
-//                 background: 'black',
-//                 color: 'white'
-//               }
-//             }}
-//           />
-//           {children}
-//         </NextUIProvider>
-//       </UIContext.Provider>
-//     );
-//   };
-//   export { UIContext, UIProvider };
+import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
+import { createContext, useState, useEffect } from 'react';
+import { Button, NextUIProvider } from '@nextui-org/react';
+import toast, { Toaster } from 'react-hot-toast';
+import { useNavigate } from "react-router-dom";
+import Menu from './Menu';
+const UIContext = createContext(undefined);
+const UIProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
+    useEffect(() => {
+        // Fermer le menu automatiquement si l'utilisateur est sur la page StartGame
+        if (location.pathname === '/startGame') {
+            setIsMenuOpen(false);
+        }
+    }, [location]);
+    useEffect(() => {
+        if (isMenuOpen) {
+            document.body.classList.add('no-scroll');
+        }
+        else {
+            document.body.classList.remove('no-scroll');
+        }
+        // Nettoyage lors du démontage du composant
+        return () => {
+            document.body.classList.remove('no-scroll');
+        };
+    }, [isMenuOpen]);
+    const notify = (type, content, options, promiseOptions) => {
+        const toastOptions = { ...options };
+        if (type === 'success') {
+            return toast.success(content, toastOptions);
+        }
+        if (type === 'error') {
+            return toast.error(content, toastOptions);
+        }
+        if (type === 'loading') {
+            return toast.loading(content, toastOptions);
+        }
+        return toast.custom(content, toastOptions);
+    };
+    const handleNavigate = (path) => {
+        navigate(path);
+        setIsMenuOpen(false);
+    };
+    return (_jsx(UIContext.Provider, { value: { notify }, children: _jsxs(NextUIProvider, { children: [isMenuOpen && (_jsxs(_Fragment, { children: [_jsx("div", { className: "overlay", onClick: () => setIsMenuOpen(false) }), " ", _jsx(Menu, { handleNavigate: handleNavigate, setIsMenuOpen: setIsMenuOpen })] })), _jsx(Button, { color: 'warning', onClick: () => setIsMenuOpen(!isMenuOpen), style: {
+                        position: 'fixed',
+                        top: '10px',
+                        right: '100px',
+                        zIndex: 1000
+                    }, children: "Menu" }), _jsx(Toaster, { toastOptions: {
+                        duration: 4000,
+                        position: 'bottom-right',
+                        style: {
+                            padding: '20px 24px',
+                            background: 'black',
+                            color: 'white'
+                        }
+                    } }), children] }) }));
+};
+export { UIContext, UIProvider };
 //# sourceMappingURL=UiProvider.js.map
