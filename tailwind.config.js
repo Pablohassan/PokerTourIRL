9e02dd5eb5144696b8bc73bfd7ee29eea4732a89
Sadikovic Rusmir <sesmanovic@gmail.com>
2024-08-24 21:52:41 +0200

    css update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+
 /** @type {import('tailwindcss').Config} */
-import { nextui } from "@nextui-org/react";
-module.exports = {
+// import { nextui } from "@nextui-org/react";
+export default {
   
   content: [
     "./index.html",
     "./src/**/*.{html, js, ts, jsx, tsx}",
-   
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
