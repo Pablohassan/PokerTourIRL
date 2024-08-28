e4efe85474a5da51cc2416c2ca71d0f489f36df2
Sadikovic Rusmir <sesmanovic@gmail.com>
2024-08-28 09:28:49 +0200

    database

--- src/backend/src/prisma/migrations/20240828071319_gamestate/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX `Party_tournamentId_fkey` ON `Party`;
-
--- DropIndex
-DROP INDEX `PlayerStats_partyId_fkey` ON `PlayerStats`;
-
--- DropIndex
-DROP INDEX `PlayerStats_playerId_fkey` ON `PlayerStats`;
--- src/backend/src/prisma/migrations/20240828072736_/migration.sql
+++ src/backend/src/prisma/migrations/20240828072736_/migration.sql
@@ -50,11 +50,12 @@ CREATE TABLE `PlayerStats` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- AddForeignKey
-ALTER TABLE `Party` ADD CONSTRAINT `Party_tournamentId_fkey` FOREIGN KEY (`tournamentId`) REFERENCES `Tournament`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateTable
+CREATE TABLE `GameState` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `state` JSON NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
--- AddForeignKey
-ALTER TABLE `PlayerStats` ADD CONSTRAINT `PlayerStats_partyId_fkey` FOREIGN KEY (`partyId`) REFERENCES `Party`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `PlayerStats` ADD CONSTRAINT `PlayerStats_playerId_fkey` FOREIGN KEY (`playerId`) REFERENCES `Player`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
