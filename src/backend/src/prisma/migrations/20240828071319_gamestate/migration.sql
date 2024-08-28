a3ad7f60102ce19174a8b1cf387fb5bb0f396504
Sadikovic Rusmir <sesmanovic@gmail.com>
2024-08-28 09:13:53 +0200

    modif game state

---
+++ src/backend/src/prisma/migrations/20240828071319_gamestate/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX `Party_tournamentId_fkey` ON `Party`;
+
+-- DropIndex
+DROP INDEX `PlayerStats_partyId_fkey` ON `PlayerStats`;
+
+-- DropIndex
+DROP INDEX `PlayerStats_playerId_fkey` ON `PlayerStats`;
