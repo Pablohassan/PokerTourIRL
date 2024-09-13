d1831c143bfcb3cdc6245e1f72cd9a4243326568
Sadikovic Rusmir <sesmanovic@gmail.com>
2024-09-13 23:09:39 +0200

    update react version

--- build/components/Content.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export const Content = () => {
-    return (_jsx("div", { style: { fontSize: "20px", marginTop: "8px", fontFamily: "DS-DIGI" } }));
-};
--- build/components/Menu.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-const Menu = ({ handleNavigate, setIsMenuOpen }) => {
-    const handleClick = (path) => {
-        handleNavigate(path);
-        setIsMenuOpen(false);
-    };
-    return (_jsx("div", { className: "menu", children: _jsxs("div", { className: "button-container", children: [_jsx("button", { className: "poker-button", onClick: () => handleClick("/ranking"), children: "Ranking" }), _jsx("button", { className: "poker-button", onClick: () => handleClick("/startGame"), children: "Start Partie" }), _jsx("button", { className: "poker-button", onClick: () => handleClick("/results"), children: "Results" }), _jsx("button", { className: "poker-button", onClick: () => handleClick("/addplayer"), children: "Add Player" }), _jsx("button", { className: "poker-button", onClick: () => handleClick("/partypage"), children: "All Parties" }), _jsx("div", { className: "sign-out-button" })] }) }));
-};
-export default Menu;
