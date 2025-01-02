cb3b86b301f09d15b27e4d422f6bec716d639e1f
Sadikovic Rusmir <sesmanovic@gmail.com>
2025-01-02 19:27:03 +0100

    css'

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -7,6 +7,7 @@ interface PlayerListProps {
   games: PlayerStats[];
   handleRebuy: (playerId: number) => void;
   handleOutOfGame: (partyId: number, playerId: number, eliminatedById: number | null) => void;
+  style?: React.CSSProperties;
 }
 
 const PlayerList: React.FC<PlayerListProps> = ({ players, games, handleRebuy, handleOutOfGame }) => {
